2a798d5113bb62c4be82978ac91fd426c01c9d7b
earbaj <earbajsaria3@gmail.com>
2025-03-01 09:40:08 -0800

    Initial Commit

---
+++ haloween/haloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  haloweenApp.swift
+//  haloween
+//
+//  Created by Earbaj Md Saria on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct haloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ haloweenTests/haloweenTests.swift
@@ -0,0 +1,17 @@
+//
+//  haloweenTests.swift
+//  haloweenTests
+//
+//  Created by Earbaj Md Saria on 1/3/25.
+//
+
+import Testing
+@testable import haloween
+
+struct haloweenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
